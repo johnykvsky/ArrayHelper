f4482ab803eb5946c2749b9b45d28c7a2f0a1ec3
johnykvsky <johnykvsky@protonmail.com>
2020-06-03 22:54:14 +0200

    Refactored. Added methods. Updated tests.

--- src/ArrayHelper.php
+++ src/ArrayHelper.php
@@ -7,6 +7,22 @@ namespace johnykvsky\Utils;
 class ArrayHelper
 {
     /**
+     * Merge two arrays
+     * @param mixed[] $array1 First array
+     * @param mixed[] $array2 Second array
+     * @param boolean $deep If false, values that are arrays are not merged, but replaced by $array2 values
+     * @return mixed[]
+     */
+    public static function merge(array $array1, array $array2, bool $deep = true): array
+    {
+        return (static::isAssoc($array2)) ? static::mergeAssociative(
+            $array1,
+            $array2,
+            $deep
+        ) : static::mergeAssociative($array1, $array2);
+    }
+
+    /**
      * Check if $array is associative
      * @param mixed[] $array Array to be checked
      * @return boolean
@@ -28,28 +44,72 @@ class ArrayHelper
      * @param boolean $deep If false, values that are arrays are not merged, but replaced by $array2 values
      * @return mixed[]
      */
-    public static function merge(array $array1, array $array2, bool $deep = true): array
+    public static function mergeAssociative(array $array1, array $array2, bool $deep = true): array
     {
-        if (static::isAssoc($array2)) {
-            foreach ($array2 as $key => $value) {
-                if (is_array($value) && isset($array1[$key]) && is_array($array1[$key]) && $deep) {
-                    $array1[$key] = static::merge($array1[$key], $value, $deep);
-                } else {
-                    if (is_int($key)) {
-                        $array1[] = $value;
-                    } else {
-                        $array1[$key] = $value;
-                    }
-                }
-            }
-        } else {
-            foreach ($array2 as $value) {
-                if (!in_array($value, $array1, true)) {
+        foreach ($array2 as $key => $value) {
+            if (is_array($value) && isset($array1[$key]) && is_array($array1[$key]) && $deep) {
+                $array1[$key] = static::merge($array1[$key], $value, $deep);
+            } else {
+                if (is_int($key)) {
                     $array1[] = $value;
+                } else {
+                    $array1[$key] = $value;
                 }
             }
         }
 
         return $array1;
+    }
+
+    /**
+     * Merge two arrays
+     * @param mixed[] $array1 First array
+     * @param mixed[] $array2 Second array
+     * @return mixed[]
+     */
+    public static function mergeNonAssociative(array $array1, array $array2): array
+    {
+        foreach ($array2 as $value) {
+            if (!in_array($value, $array1, true)) {
+                $array1[] = $value;
+            }
+        }
+
+        return $array1;
+    }
+
+    /**
+     * Join two arrays
+     * @param mixed[] $array1 First array
+     * @param mixed[] $array2 Second array
+     * @return mixed[]
+     */
+    public static function combine(array $array1, array $array2): array
+    {
+        foreach ($array2 as $value) {
+            $array1[] = $value;
+        }
+
+        return $array1;
+    }
+
+    /**
+     * Get first element from array (value), without modifying an array (like array_pop)
+     * @param array $array Input array
+     * @return mixed|null
+     */
+    public static function firstValue(array $array)
+    {
+        return current(array_slice($array, 0));
+    }
+
+    /**
+     * Get last element from array (value), without modifying an array
+     * @param array $array Input array
+     * @return mixed|null
+     */
+    public static function lastValue(array $array)
+    {
+        return current(array_slice($array, -1));
     }
 }
--- tests/ArrayHelperTest.php
+++ tests/ArrayHelperTest.php
@@ -6,52 +6,86 @@ use PHPUnit\Framework\TestCase;
 
 class ArrayHelperTest extends TestCase
 {
-    private $inputArray = ['johny'=>['age'=>30,'weight'=>70],'chris'=>['height'=>170]];
-    private $updateArray = ['hobby'=>[['music'=>'rock']]];
+    private $inputArray = ['johny' => ['age' => 30, 'weight' => 70], 'chris' => ['height' => 170]];
+    private $updateArray = ['hobby' => [['music' => 'rock']]];
 
     public function testMerge()
     {
         $result = johnykvsky\Utils\ArrayHelper::merge($this->inputArray, $this->updateArray);
-        $expected = ['johny'=>['age'=>30,'weight'=>'70'],'chris'=>['height'=>170],'hobby'=>[['music'=>'rock']]];
+        $expected = [
+            'johny' => ['age' => 30, 'weight' => '70'],
+            'chris' => ['height' => 170],
+            'hobby' => [['music' => 'rock']],
+        ];
         $this->assertEquals($expected, $result);
     }
 
     public function testIsAssocTrue()
     {
-        $array = array('foo' => 'bar');
-        $this->assertEquals(johnykvsky\Utils\ArrayHelper::isAssoc($array), true);
+        $array = ['foo' => 'bar'];
+        $this->assertEquals(true, johnykvsky\Utils\ArrayHelper::isAssoc($array));
+    }
+
+    public function testCombine()
+    {
+        $array = ['foo' => 'bar'];
+        $array2 = ['foo2' => 'bar2'];
+        $this->assertEquals(['foo' => 'bar', 'bar2'], johnykvsky\Utils\ArrayHelper::combine($array, $array2));
+    }
+
+    public function testFirstValue()
+    {
+        $array = ['foo' => 'bar', 'foo2' => 'bar2'];
+        $this->assertEquals('bar', johnykvsky\Utils\ArrayHelper::firstValue($array));
+        $this->assertEquals(['foo' => 'bar', 'foo2' => 'bar2'], $array);
+    }
+
+    public function testLastValue()
+    {
+        $array = ['foo' => 'bar', 'foo2' => 'bar2'];
+        $this->assertEquals('bar2', johnykvsky\Utils\ArrayHelper::lastValue($array));
+        $this->assertEquals(['foo' => 'bar', 'foo2' => 'bar2'], $array);
     }
 
     public function testIsAssocFalse()
     {
-        $array = array('foo', 'bar');
-        $this->assertEquals(johnykvsky\Utils\ArrayHelper::isAssoc($array), false);
+        $array = ['foo', 'bar'];
+        $this->assertEquals(false, johnykvsky\Utils\ArrayHelper::isAssoc($array));
     }
 
     public function testMergeNotAssociative()
     {
         $array1 = ['foo' => 'bar'];
-        $array2 = [3,5,7];
-        $result = johnykvsky\Utils\ArrayHelper::merge($array1, $array2);
+        $array2 = [3, 5, 7];
+        $result = johnykvsky\Utils\ArrayHelper::mergeNonAssociative($array1, $array2);
         $expected = ['foo' => 'bar', 3, 5, 7];
         $this->assertEquals($expected, $result);
     }
 
-    public function testDeepMerge()
+    public function testMergeAssociativeDeepMerge()
     {
         $array1 = ['foo' => ['arj' => 'pak']];
         $array2 = ['foo' => ['tar' => 'zip']];
-        $result = johnykvsky\Utils\ArrayHelper::merge($array1, $array2);
+        $result = johnykvsky\Utils\ArrayHelper::mergeAssociative($array1, $array2);
         $expected = ['foo' => ['arj' => 'pak', 'tar' => 'zip']];
         $this->assertEquals($expected, $result);
     }
 
-    public function testNonDeepMerge()
+    public function testMergeAssociativeNonDeepMerge()
     {
         $array1 = ['foo' => ['arj' => 'pak']];
         $array2 = ['foo' => ['tar' => 'zip']];
-        $result = johnykvsky\Utils\ArrayHelper::merge($array1, $array2, false);
+        $result = johnykvsky\Utils\ArrayHelper::mergeAssociative($array1, $array2, false);
         $expected = ['foo' => ['tar' => 'zip']];
+        $this->assertEquals($expected, $result);
+    }
+
+    public function testMergeAssociativeWithoutKeys()
+    {
+        $array1 = ['foo', 'arj', 'pak'];
+        $array2 = ['bar', 'tar', 'zip'];
+        $result = johnykvsky\Utils\ArrayHelper::mergeAssociative($array1, $array2);
+        $expected = ['foo', 'arj', 'pak', 'bar', 'tar', 'zip'];
         $this->assertEquals($expected, $result);
     }
 }
